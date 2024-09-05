3cffc8d4ca320e61d33846d82a32ff000de3140f
asheq1 <bsmrstu18fmb025@gmail.com>
2024-09-05 07:30:47 +0600

    add todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import './App.css'
+import TodoForm from './components/TodoForm/TodoForm'
+import TodoList from './components/TodoList/TodoList'
 import './styles/App.css'
 
 
 function App() {
-  
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, isComplete: false}]
+    setTodos(newTodos)
+  }
+
+
   return (
     <>
       <div className='App'>
           <h1>Todo App</h1>
-
+          <TodoForm addTodo={addTodo}></TodoForm>
+          <TodoList></TodoList>
       </div>
     </>
   )
--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,10 +1,48 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const TodoForm = () => {
+const TodoForm = ({addTodo}) => {
+    const [todo, setTodo] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = e =>{
+        e.preventDefault();
+        
+        if(!todo.trim()){
+            setError('Todo cannot be empty or only spaces!')
+            return;
+        }
+
+        // clear the error and add the todo
+        setError('')
+        addTodo(todo)
+        setTodo('') // clear the input
+
+    };
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="text"
+                    value={todo}
+                    onChange ={(e)=> setTodo(e.target.value)}
+                    placeholder='Add a new task...'
+                    />
+                <button type="submit">Add</button>
+            </form>
+
+            {/* Display error message if come  */}
+            {error && <p style={{color: 'red'}}>{error}</p>}
         </div>
     );
 };
+
+
+TodoForm.proptypes = {
+    
+
+}
+
 
 export default TodoForm;
